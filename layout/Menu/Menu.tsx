d3eb9ab1456d85a8e12f0029654ff0e8be822525
Sergei Zhilinskij <roadtomars2030@gmail.com>
2022-10-24 16:56:40 +0400

    add routing

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,49 +1,17 @@
 import styles from './Menu.module.css';
 import cn from 'classnames';
 import { FC, useContext } from 'react';
-// import { format } from 'date-fns';
 import { AppContext } from '../../context/app.context';
-import BooksIcon from './icons/books.svg';
-import CoursesIcon from './icons/courses.svg';
-import ProductsIcon from './icons/products.svg';
-import ServicesIcon from './icons/services.svg';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
-import { TopLevelCategory } from '../../interfaces/page.interface';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-  {
-    route: 'courses',
-    name: 'Курсы',
-    icon: <CoursesIcon />,
-    id: TopLevelCategory.Courses,
-  },
-  {
-    route: 'services',
-    name: 'Сервисы',
-    icon: <ServicesIcon />,
-    id: TopLevelCategory.Services,
-  },
-  {
-    route: 'books',
-    name: 'Книги',
-    icon: <BooksIcon />,
-    id: TopLevelCategory.Books,
-  },
-  {
-    route: 'products',
-    name: 'Продукты',
-    icon: <ProductsIcon />,
-    id: TopLevelCategory.Products,
-  },
-];
+import { firstLevelMenu } from '../../helpers/helpers';
 
 export const Menu: FC = () => {
   const { menu, firstCategory, setMenu } = useContext(AppContext);
   const router = useRouter();
 
-  const openSecondLevel = (secondCategory: string) => {
+  const openSecondLevel = (secondCategory: string): void => {
     setMenu &&
       setMenu(
         menu.map((m) => {
--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -3,10 +3,13 @@ import styles from './Sidebar.module.css';
 import cn from 'classnames';
 import { FC } from 'react';
 import { Menu } from '../Menu/Menu';
+import Logo from '../logo.svg';
 
-export const Sidebar: FC<SidebarProps> = ({ ...props }) => {
+export const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div className={cn(styles.search)}>Search</div>
       <Menu />
     </div>
   );
